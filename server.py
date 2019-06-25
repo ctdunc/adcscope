e89ab701e67114fba788309efe44cd43f1df7488
ctdunc <connor.t.duncan@gmail.com>
2019-06-25 11:58:30 -0700

    fixing overwrite error

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from celery import Celery
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import numpy as np
@@ -6,14 +7,19 @@ import eventlet
 
 app = Flask(__name__, static_folder='./view/dist', template_folder='./view')
 app.config['SECRET_KEY'] = 'secret'
+app.config["CELERY_BROKER_URL"] = "redis://localhost:6379/0"
+app.config["CELERY_RESULT_BACKEND"] = "redis://localhost:6379/0"
+
+celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
+celery.conf.update(app.config)
 
 socketio = SocketIO()
 socketio.init_app(app,async_mode='eventlet',message_queue="redis://")
 eventlet.monkey_patch()
+
 @app.route("/")
 def render():
     return render_template('index.html')
 
 if __name__ == '__main__':
     socketio.run(app)
-    print('addd')
