0ef9a8d631c5af5ffb017d646fd9ce0a0849bf69
ctdunc <connor.t.duncan@gmail.com>
2019-07-26 10:55:15 -0700

    beginning of control panel, update flask to use Blueprint()

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
 import numpy as np
 import datetime
 from gevent import monkey
+from scope_blueprint import oscope
+from control_blueprint import control
 
 monkey.patch_all()
-app = Flask(__name__, static_folder='./view/dist', template_folder='./view')
+app = Flask(__name__)
+app.register_blueprint(oscope, url_prefix='/scope')
+app.register_blueprint(control, url_prefix='/control')
 app.config['SECRET_KEY'] = 'secret'
 
 socketio = SocketIO()
 socketio.init_app(app, message_queue="redis://")
-
-@app.route("/")
+"""
+@app.route("/scope")
 def render():
-    return render_template('index.html')
-
+    return render_template('scope/scope.html')
+"""
 
 if __name__ == '__main__':
     socketio.run(app)
