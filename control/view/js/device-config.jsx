e92107e87a93d45c117afc906723244275ccd3fb
ctdunc <connor.t.duncan@gmail.com>
2019-08-02 12:27:32 -0700

    updates to menu

--- control/view/js/device-config.jsx
+++ control/view/js/device-config.jsx
@@ -1,27 +1,65 @@
 import React, { Component } from 'react';
+import { Select, MenuItem } from "@material-ui/core";
 
 export default class DeviceConfig extends Component{
 	constructor(props,context){
 		super(props,context);
+		this.state = {};
 		this.makeConfigByChannelType = this.makeConfigByChannelType.bind(this);
+		this.makeCentralConfigPane = this.makeCentralConfigPane.bind(this);
+		this.handleChannelModeChange = this.handleChannelModeChange.bind(this);
+		this.handleTriggerModeChange = this.handleTriggerModeChange.bind(this);
 	}
 
+	componentDidUpdate(prevProps){
+		// updates channels from default value after parent GET request for info succeeds
+		if(prevProps!==this.props){
+			var channels = [];
+			Object.entries(this.props.conf_vars).map((entry) => {
+				channels.push(...entry[1].channels);
+			})
+			channels.map(c => {
+				this.setState({[c]: {mode: "off", trigger: false}})
+			})
+		}
+	}
 	makeConfigByChannelType(type, options){
 		return(
-			<div className="channel-type-config" key={type}>
+			<div key={type}>
 				<h3> {type} </h3>
+				<div className="channel-config">
+					<b className="title"> General Options </b>
+				</div>
 				{options.channels.map((c)=>{
 					return(
-						<div key={c}>
-						/* TODO each channel needs
-						 *	- mode (read/write, if enabled)
-						 *	- enabled (set read/write/none)
-						 *	- trigger
-						 *	- role (i.e. aux trigger orsomething, might b nice for metadata).
-						 */	
-						<b className="channel-label">{c}</b>
-						<input type="checkbox" className="enable-channel"/>
-						<input type="checkbox" className="is-trigger"/>
+						<div key={c} className="channel-config">
+								<div className="title">
+									<b> {c} </b>
+								</div>
+								<div className="mode">
+									<Select
+									value={this.state[c] ? this.state[c].mode: "off"}
+									autoWidth={true}
+									inputProps={{name:c, id: "channel"}}
+									onChange={this.handleChannelModeChange}	
+									>
+										<MenuItem value="off">Off  </MenuItem>
+										<MenuItem value="read">Read  </MenuItem>
+										<MenuItem value="write">Write</MenuItem>
+									</Select>
+								</div>
+								<div className="trigger">	
+									<label htmlFor="trigger">Is Trigger</label>
+									<input 
+									type="radio" 
+									name="trigger" 
+									value={c} 
+									onChange={this.handleTriggerModeChange}/>
+								</div>
+								<div>	
+									<label htmlFor="chan-notes">Notes</label>
+									<input id="chan-notes"/>
+								</div>
 						</div>
 					);
 				})}
@@ -29,11 +67,44 @@ export default class DeviceConfig extends Component{
 		);
 	}
 
-	setChannelAsTrigger(event){
-		// TODO: write
+	makeCentralConfigPane(){
+		var conf_vars = this.props.conf_vars;
+		if(conf_vars.is_currently_running){
+			return(
+				<div className={this.props.className}> Device is running! Please stop it before attempting to configure run settings </div>
+			);
+		}else{
+			delete conf_vars.is_currently_running;
+			return(
+				<div className={this.props.className}>
+				{Object.keys(conf_vars).map(k=>
+					{return(this.makeConfigByChannelType(k, conf_vars[k]))}
+				)}
+				</div>
+			);
+		}
+	}
+	handleTriggerModeChange(event){
+		const channels = Object.keys(this.state);
+		const index = channels.indexOf(event.target.value);
+		if(index > -1){
+			const c = channels[index]
+			var cstate = this.state[c]
+			cstate.mode = "read"
+			this.setState({[c]: cstate, trig: event.target.value});
+		}
+		else{
+			this.setState({trig: event.target.value});
+		}
+			
 	}
 
-	toggleChannel
+	handleChannelModeChange(event){
+		var toUpdate = this.state[event.target.name];
+		toUpdate.mode = event.target.value;
+		this.setState({[event.target.name]: toUpdate});
+	}
+
 
 	render(){
 		var conf_opts = this.props.conf_vars;
@@ -43,8 +114,9 @@ export default class DeviceConfig extends Component{
 			delete conf_opts.is_currently_running;
 		return(
 			<div className={this.props.className}> 
-			apt
-			{Object.keys(conf_opts).map(k => {return(this.makeConfigByChannelType(k, conf_opts[k]))})} 
+				<div className="channel-config-container">
+					{this.makeCentralConfigPane()}
+				</div>
 			</div>
 		);
 		}
