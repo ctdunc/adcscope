f273873bde924477124303f59f23d22ead6c30e2
Connor Duncan <connor.t.duncan@gmail.com>
2019-08-06 10:01:35 -0700

    commit blueprint control before merge from master

--- control/blueprint.py
+++ control/blueprint.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
-
+import redis
+r = redis.Redis()
 control = Blueprint(
         "control",
         __name__,
         static_folder="./view/dist",
         template_folder='./view'
         )
-
+@control.route("/get-dev")
+def get_dev():
+    print(r.keys())
+    return("hell")
 @control.route("/")
 def render():
     return render_template("index.html")
